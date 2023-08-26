4ad1903b8fc621f3724c52c6cf0bbbe1bc56db51
Theo <kkw.theo@gmail.com>
2023-08-26 21:19:13 +0900

    user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,27 @@
 import { JwtService } from '@nestjs/jwt';
-import { SignIn, SignUp } from './dto/auth.dto';
+import {
+  Response,
+  SignIn,
+  SignInResponse,
+  SignUp,
+  SignUpResponse,
+} from './dto/auth.dto';
 import { UsersService } from './users.service';
 import {
   Body,
   ConflictException,
   Controller,
+  HttpCode,
   Post,
   UnauthorizedException,
 } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
@@ -17,6 +30,15 @@ export class UsersController {
     private readonly jwtService: JwtService,
   ) {}
   @Post('/sign_up')
+  @ApiOperation({ summary: '유저 생성 API', description: '유저를 생성한다.' })
+  @ApiCreatedResponse({
+    description: '유저를 생성한다.',
+    type: Response<SignUpResponse>,
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Server Error',
+  })
   async signUp(@Body() body: SignUp) {
     const { loginId, password, nickname } = body;
 
@@ -47,13 +69,21 @@ export class UsersController {
     return {
       status: true,
       result: {
-        ...userEntity,
-        password: undefined,
+        message: '회원가입이 되었습니다.',
       },
     };
   }
 
   @Post('/sign_in')
+  @HttpCode(200)
+  @ApiOkResponse({
+    description: '로그인.',
+    type: Response<SignInResponse>,
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Server Error',
+  })
   async signIn(@Body() body: SignIn) {
     const { loginId, password } = body;
 
